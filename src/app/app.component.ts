207740936142208392bc5cd3000a94a63cfab445
Sleyter <sleyter94@gmail.com>
2021-05-28 22:52:51 -0500

    adding primeng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipesApp';
+  nombre: string = 'Sleyter';
+  mostrarNombre() {
+    console.log(this.nombre);
+  }
 }
