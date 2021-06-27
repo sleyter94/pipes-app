794b8e02446f3a97793072f4fe0ac07e027525e4
Sleyter <sleyter94@gmail.com>
2021-06-26 23:54:47 -0500

    adding routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {ButtonModule} from 'primeng/button';
 
 import { AppComponent } from './app.component';
+import { PrimeNgModule } from './prime-ng/prime-ng.module';
+import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    ButtonModule
+    SharedModule,
+    PrimeNgModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
